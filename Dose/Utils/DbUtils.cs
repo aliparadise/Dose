0e9e6d97c7e7a6f50bd06ebde0670107d89904f6
Ali Paradise <alison.paradise@gmail.com>
2022-04-29 09:57:34 -0500

    Added dbUtils

---
+++ Dose/Utils/DbUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace Dose.Utils
+{
+    public static class DbUtils
+    {
+        public static string GetNullableString(SqlDataReader reader, string column)
+        {
+            var ordinal = reader.GetOrdinal(column);
+            if (reader.IsDBNull(ordinal))
+            {
+                return null;
+            }
+            return reader.GetString(ordinal);
+        }
+    }
+}
